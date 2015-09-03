7857c1ac67ae1d64948735883a9af9efbb3c1cb3
Matthias L. Jugel <matthias.jugel@streamdrill.com>
2015-09-03 19:37:21 +0200

    use some state

--- sketches/switch/switch.cpp
+++ sketches/switch/switch.cpp
@@ -27,7 +27,181 @@ Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 #define PIN_LED 13
 #define PIN_BUTTON A0
 
-int switchState = 0;
+//Symbols for LoF
+static const uint8_t PROGMEM
+
+        logothgs_bmp[] =
+        {B00000000,
+         B00100100,
+         B00100100,
+         B01111110,
+         B00100100,
+         B00100100,
+         B00000000,
+         B00000000},
+        logoLOF_bmp[] =
+        {B00000000,
+         B00000000,
+         B00111100,
+         B00000100,
+         B00000100,
+         B00000100,
+         B00000000,
+         B00000000},
+        OSN_bmp[] =
+        {B01100111,
+         B10010100,
+         B10010111,
+         B10010001,
+         B01100111,
+         B00000000,
+         B01111110,
+         B00000000},
+        x_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B10000000},
+        y_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B11100000,
+         B00100000,
+         B10100000},
+        z_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B11000000,
+         B11000000},
+        y2_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B11110000,
+         B00010000,
+         B11010000,
+         B11010000},
+        x3_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B11100000,
+         B11100000,
+         B11100000},
+        y3_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B11111000,
+         B00001000,
+         B11101000,
+         B11101000,
+         B11101000},
+        x4_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B00000000,
+         B11110000,
+         B11110000,
+         B11110000,
+         B11110000},
+        y4_bmp[] =
+        {B00000000,
+         B00000000,
+         B11111100,
+         B00000100,
+         B11110100,
+         B11110100,
+         B11110100,
+         B11110100},
+        x5_bmp[] =
+        {B00000000,
+         B00000000,
+         B00000000,
+         B11111000,
+         B11111000,
+         B11111000,
+         B11111000,
+         B11111000},
+        y5_bmp[] =
+        {B00000000,
+         B11111110,
+         B00000010,
+         B11111010,
+         B11111010,
+         B11111010,
+         B11111010,
+         B11111010},
+        x6_bmp[] =
+        {B00000000,
+         B00000000,
+         B11111100,
+         B11111100,
+         B11111100,
+         B11111100,
+         B11111100,
+         B11111100},
+        y6_bmp[] =
+        {B11111111,
+         B00000001,
+         B11111101,
+         B11111101,
+         B11111101,
+         B11111101,
+         B11111101,
+         B11111101},
+        x7_bmp[] =
+        {B00000000,
+         B11111110,
+         B11111110,
+         B11111110,
+         B11111110,
+         B11111110,
+         B11111110,
+         B11111110},
+        z2_bmp[] =
+        {B11111111,
+         B11111111,
+         B11111111,
+         B11111111,
+         B11111111,
+         B11111111,
+         B11111111,
+         B11111111},
+        logothgsinvers_bmp[] =
+        {B11111111,
+         B11011011,
+         B11011011,
+         B10000001,
+         B11011011,
+         B11011011,
+         B11111111,
+         B11111111},
+        logoLOFinvers_bmp[] =
+        {B11111111,
+         B11111111,
+         B11000011,
+         B11111011,
+         B11111011,
+         B11111011,
+         B11111111,
+         B11111111};
+
 
 
 void setup() {
@@ -37,185 +211,14 @@ void setup() {
 
     pinMode(PIN_LED, OUTPUT);
     pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+    // initialize with the first image
+    matrix.clear();
+    matrix.drawBitmap(0, 0, logoLOF_bmp, 8, 8, LED_ON);
+    matrix.writeDisplay();
 }
 
-
-//Symbols for LoF
-static const uint8_t PROGMEM
-
-        logothgs_bmp[] =
-        { B00000000,
-          B00100100,
-          B00100100,
-          B01111110,
-          B00100100,
-          B00100100,
-          B00000000,
-          B00000000 },
-        logoLOF_bmp[] =
-        { B00000000,
-          B00000000,
-          B00111100,
-          B00000100,
-          B00000100,
-          B00000100,
-          B00000000,
-          B00000000 },
-        OSN_bmp[] =
-        { B01100111,
-          B10010100,
-          B10010111,
-          B10010001,
-          B01100111,
-          B00000000,
-          B01111110,
-          B00000000 },
-        x_bmp[] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B10000000 },
-        y_bmp[] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B11100000,
-          B00100000,
-          B10100000 },
-        z_bmp[] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B11000000,
-          B11000000 },
-        y2_bmp[] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B11110000,
-          B00010000,
-          B11010000,
-          B11010000 },
-        x3_bmp[] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B11100000,
-          B11100000,
-          B11100000 },
-        y3_bmp[] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B11111000,
-          B00001000,
-          B11101000,
-          B11101000,
-          B11101000 },
-        x4_bmp [] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B00000000,
-          B11110000,
-          B11110000,
-          B11110000,
-          B11110000 },
-        y4_bmp [] =
-        { B00000000,
-          B00000000,
-          B11111100,
-          B00000100,
-          B11110100,
-          B11110100,
-          B11110100,
-          B11110100 },
-        x5_bmp [] =
-        { B00000000,
-          B00000000,
-          B00000000,
-          B11111000,
-          B11111000,
-          B11111000,
-          B11111000,
-          B11111000 },
-        y5_bmp [] =
-        { B00000000,
-          B11111110,
-          B00000010,
-          B11111010,
-          B11111010,
-          B11111010,
-          B11111010,
-          B11111010 },
-        x6_bmp [] =
-        { B00000000,
-          B00000000,
-          B11111100,
-          B11111100,
-          B11111100,
-          B11111100,
-          B11111100,
-          B11111100 },
-        y6_bmp [] =
-        { B11111111,
-          B00000001,
-          B11111101,
-          B11111101,
-          B11111101,
-          B11111101,
-          B11111101,
-          B11111101 },
-        x7_bmp [] =
-        { B00000000,
-          B11111110,
-          B11111110,
-          B11111110,
-          B11111110,
-          B11111110,
-          B11111110,
-          B11111110 },
-        z2_bmp [] =
-        { B11111111,
-          B11111111,
-          B11111111,
-          B11111111,
-          B11111111,
-          B11111111,
-          B11111111,
-          B11111111 },
-        logothgsinvers_bmp [] =
-        { B11111111,
-          B11011011,
-          B11011011,
-          B10000001,
-          B11011011,
-          B11011011,
-          B11111111,
-          B11111111 },
-        logoLOFinvers_bmp [] =
-        { B11111111,
-          B11111111,
-          B11000011,
-          B11111011,
-          B11111011,
-          B11111011,
-          B11111111,
-          B11111111 };
-
-
+int bmpState = 0;
 
 void loop() {
 
@@ -223,46 +226,30 @@ void loop() {
     switchState = digitalRead(PIN_BUTTON);
     //pin3
 
-    Serial.println(switchState);
-
-    // if the switch is in state high it is pressed
+    // if the switch is in state high it is pressed, do something
     if (switchState == LOW) {
-        // do something if it is pressed, else ignore it
-
         digitalWrite(PIN_LED, HIGH);
+
         matrix.clear();
-        matrix.drawBitmap(0, 0, logoLOF_bmp, 8, 8, LED_ON);
+        // ++bmpState increases the bmp
+        switch (++bmpState) {
+            case 1:
+                matrix.drawBitmap(0, 0, OSN_bmp, 8, 8, LED_ON);
+                break;
+            case 2:
+                matrix.drawBitmap(0, 0, x_bmp, 8, 8, LED_ON);
+                break;
+            case 3:
+                matrix.drawBitmap(0, 0, y_bmp, 8, 8, LED_ON);
+                break;
+            default:
+                bmpState = 0;
+                matrix.drawBitmap(0, 0, logoLOF_bmp, 8, 8, LED_ON);
+        }
         matrix.writeDisplay();
-    }
 
-    // if the switch is in state high it is pressed
-    if (switchState == HIGH) {
-        // do something if it is pressed, else ignore it
-
-        digitalWrite(PIN_LED, LOW);
-        matrix.clear();
-        matrix.drawBitmap(0, 0, OSN_bmp, 8, 8, LED_ON);
-        matrix.writeDisplay();
-    }
-
-
-    // if the switch is in state high it is pressed
-    if (switchState == LOW) {
-        // do something if it is pressed, else ignore it
-
+    } else {
         digitalWrite(PIN_LED, HIGH);
-        matrix.clear();
-        matrix.drawBitmap(0, 0, x_bmp, 8, 8, LED_ON);
-        matrix.writeDisplay();
     }
 
-    // if the switch is in state high it is pressed
-    if (switchState == HIGH) {
-        // do something if it is pressed, else ignore it
-
-        digitalWrite(PIN_LED, LOW);
-        matrix.clear();
-        matrix.drawBitmap(0, 0, y_bmp, 8, 8, LED_ON);
-        matrix.writeDisplay();
-    }
 }
